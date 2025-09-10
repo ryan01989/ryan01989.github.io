8f8a0e71f77fd13e82913fd3ffd536e765701cef
Dylan_Ryan <ryan.1989@osu.edu>
2025-09-10 12:40:41 -0400

    continued ga support. remove vite logo

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-GNJ6ZE9LG0"); // your GA ID
+ReactGA.send("pageview");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
